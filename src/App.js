d5770a14199e9189a7325a90863186867fa3779f
FAYSAL4525 <faysalahamed306@gmail.com>
2021-06-11 11:37:23 +0600

    create navbar & home page by tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Dropdown from "./components/Dropdown";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Contacs from "./pages/Contacs";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
 
 function App() {
+  const [isOpen, setIsopen] = useState(false);
+
+  const toggle = () => {
+    setIsopen(!isOpen);
+  };
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth > setIsopen(false)) {
+        console.log("resizeed");
+      }
+    };
+    window.addEventListener("resize", hideMenu);
+    return () => {
+      window.removeEventListener("resize", hideMenu);
+    };
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
+        <Route path="/menu" exact component={Menu} />
+        <Route path="/contacs" exact component={Contacs} />
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/pages/Contacs.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Contacs = () => {
+  return (
+    <>
+      <div className="h-screen flex justify-center items-center bg-yellow-300">
+        <h1 className="lg:text-9xl md:text-7xl sm:text-5xl text-3xl uppercase font-black">
+          contacts Page
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default Contacs;
